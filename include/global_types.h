4d2db556da4d00fe9b3cf6657bdc960ed1c6d24b
kimyoungsik-gif <addi97@naver.com>
2020-01-31 14:04:22 +0900

    header

--- include/global_types.h
+++ include/global_types.h
@@ -2,9 +2,26 @@
 #define __GLOBAL_TYPES__
 
 #include <stdint.h> 
-
+#include <vector>
 #define BLOCK_SIZE	4096
 #define PAGESIZE	4096
-#define DEVSIZE 1024L * 1024L * 1024L * 16L
+#define DEVSIZE 1024L * 1024L * 1024L * 10L
 
+   //start address
+#define super_node_base_idx 0
+#define inode_bitmap_base_idx 104857600
+#define block_bitmap_base_idx 209715200
+#define inode_base_idx 314572800
+#define data_block_base_idx 1.0486e9
+
+      //end address
+#define super_node_end_idx 104857600
+#define inode_bitmap_end_idx 209715200
+#define block_bitmap_end_idx 314572800
+#define inode_end_idx 1.0486e9
+#define data_block_end_idx 1.0486e10
+
+File *fd;
+
+vector<fuse_file_info> fi;_
 #endif
