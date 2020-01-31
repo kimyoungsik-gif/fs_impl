69ccebd2513afff1cccd7e2dbd6589b4e661f4cb
kimyoungsik-gif <addi97@naver.com>
2020-01-31 14:18:31 +0900

    gloabl

--- include/global_types.h
+++ include/global_types.h
@@ -7,21 +7,29 @@
 #define PAGESIZE	4096
 #define DEVSIZE 1024L * 1024L * 1024L * 10L
 
-   //start address
+//start address
 #define super_node_base_idx 0
 #define inode_bitmap_base_idx 104857600
 #define block_bitmap_base_idx 209715200
 #define inode_base_idx 314572800
 #define data_block_base_idx 1.0486e9
 
-      //end address
+//end address
 #define super_node_end_idx 104857600
 #define inode_bitmap_end_idx 209715200
 #define block_bitmap_end_idx 314572800
 #define inode_end_idx 1.0486e9
 #define data_block_end_idx 1.0486e10
 
-File *fd;
+//current offset
+struct disk_offset{
+	long long inode_bitmap_offset;
+	long long data_bitmap_offset;
+	long long inode_offset;
+	long long data_block_offset;
+}
 
+
+File *fd;
 vector<fuse_file_info> fi;_
 #endif
