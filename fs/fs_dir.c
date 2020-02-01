06a5a58b25ecc3cc81de775ae506cc1b7fa99b91
hykong99 <hykong99@dgist.ac.kr>
2020-02-01 16:24:20 +0900

    MKDIR END

--- fs/fs_dir.c
+++ fs/fs_dir.c
@@ -19,9 +19,9 @@ int fs_opendir (const char *path, struct fuse_file_info *fi) {
 	result = strtok(path,"/");
 
 	while(result != NULL) {
-		pread(fd, cur_m, sizeof(struct metadata), inode_base_idx + cur_dir_inumber * INODE_SIZE );
+		pread(fd, cur_m, sizeof(struct metadata), cur_dir_inumber);
 		cur_data_block_idx = cur_m.datablock;
-		pread(fd, cur_dir_data, sizeof(struct dir_data), data_block_base_idx + cur_data_block_idx * BLOCK_SIZE );
+		pread(fd, cur_dir_data, sizeof(struct dir_data), cur_data_block_idx);
 		
 		int flag = 0;
 		for(int i = 0; i < cur_data.child.size();i++){
@@ -44,16 +44,39 @@ int fs_opendir (const char *path, struct fuse_file_info *fi) {
 
 int fs_mkdir (const char *path, mode_t mode) {
 	char* bitmap = "1";
-	char* dir_name = "mkdir_name";
-
 	
-	//A= fs_opendir(a,b)
+	//parent_Information
+	string p_path = path;
+	int size = pathhh.size();
+	int index;
+	for (int i = size; i >= 0; i--){
+		if(pathhh[i] == "/"){
+			index = i;
+			break;
+		}
+	}
+	string my_name = p_path;
+	p_path.replace(index,size,"");
+	my_name.replace(0,index+1,"");
 
+	int p_id = fs_opendir(pathhh,NULL);
+	struct metadata p_meta;
+	pread(fd,p_meta,sizeof(struct meatdata),p_id);
+	struct dir_data d;
+	pread(fd,d,sizeof(struct dir_data),p_meta.data_ptr);
+	d.child_name[p_meta.count] = my_name;
+	d.child_inumber[p_meta.count] = inode_offset;
+	p_meta.count++;
+	P_meta.size++;
+	int P_D = pwrite(fd, d, sizeof(struct dir_data), (off_t) p.meta.data_ptr);
+	int P_I = pwrite(fd, p_meta, sizeof(struct metadata), (off_t) p_id);
+
+	//child_Information
 	struct metadata m;
 	m.mode = mode;
 	m.nlink = 1;
-	m.uid = 123;
-	m.gid = 456;
+	m.uid = USER_ID;
+	m.gid = GROUP_ID;
 	m.size = 2;
 	m.atime = 11;
 	m.ctime = 22;
@@ -62,8 +85,8 @@ int fs_mkdir (const char *path, mode_t mode) {
 	m.data_ptr = data_block_offset;
 	m.count = 0;
 
-	nw1 = pwrite(fd, bitmap, BITMAP_SIZE, (off_t) inode_bitmap_offset);
-	nw2 = pwrite(fd, m, INODE_SIZE, (off_t) inode_offset);
+	int nw1 = pwrite(fd, bitmap, BITMAP_SIZE, (off_t) inode_bitmap_offset);
+	int nw2 = pwrite(fd, m, INODE_SIZE, (off_t) inode_offset);
 
 	inode_bitmap_offset	+= BITMAP_SIZE;
 	inode_offset += INODE_SIZE;
