2ef5c7186723f710d0a594edcf1010da89f92d9c
kimyoungsik-gif <addi97@naver.com>
2020-02-05 19:34:54 +0900

    generic

--- fs/fs_generic.c
+++ fs/fs_generic.c
@@ -16,7 +16,7 @@ struct monitor *global_monitor;
 
 int fs_getattr (const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
 	struct metadata meta;
-    int ind = inode_finder(path);	
+	int ind = inode_finder(path);	
 
 	int nr1 = pread(fd, &meta, sizeof(meta), (off_t) ind);
 	stbuf->st_mode = meta.mode;
@@ -26,7 +26,7 @@ int fs_getattr (const char *path, struct stat *stbuf, struct fuse_file_info *fi)
 	stbuf->st_size = meta.size;
 	stbuf->st_ino = meta.ino;
 
-    return -ENOENT;
+	return -ENOENT;
 }
 
 int fs_utimens (const char *path, const struct timespec ts[2], struct fuse_file_info *fi) {
@@ -37,25 +37,25 @@ int fs_utimens (const char *path, const struct timespec ts[2], struct fuse_file_
 
 int fs_chmod (const char *path, mode_t mode, struct fuse_file_info *fi) {
 	struct metadata meta;
-    int ind = inode_finder(path);	
+	int ind = inode_finder(path);	
 
 	int nr1 = pread(fd, &meta, sizeof(meta), (off_t) ind);
 	meta.mode = mode;
 	int nw1 = pwrite(fd, &meta, sizeof(meta), (off_t) ind);
 
-    return 0;
+	return 0;
 }
 
 int fs_chown (const char *path, uid_t uid, gid_t gid, struct fuse_file_info *fi) {
 	struct metadata meta;
-    int ind = inode_finder(path);	
+	int ind = inode_finder(path);	
 	fi->fh = ind;
 
 	int nr1 = pread(fd, &meta, sizeof(meta), (off_t) ind);
 	meta.uid = uid;
 	meta.gid = gid;
 	int nw1 = pwrite(fd, &meta, sizeof(meta), (off_t) ind);
-    return 0;
+	return 0;
 }
 
 int fs_rename (const char *oldpath, const char *newpath, unsigned int flags) {
@@ -63,62 +63,62 @@ int fs_rename (const char *oldpath, const char *newpath, unsigned int flags) {
 	string Po_path = oldpath;
 	string old_name = oldpath;
 	if(oldpath == "/") {
-	    Po_path = oldpath;
-        old_name = oldpath;
-        root_inumber = inode_base_idx;
-    }
-    else {
-       int size = Po_path.size();
-       int index;
-       for (int i = size; i >= 0; i--){
-           if(Po_path[i] == "/"){
-               index = i;
-               break;
-           }
-       }
+		Po_path = oldpath;
+		old_name = oldpath;
+		root_inumber = inode_base_idx;
+	}
+	else {
+		int size = Po_path.size();
+		int index;
+		for (int i = size; i >= 0; i--){
+			if(Po_path[i] == "/"){
+				index = i;
+				break;
+			}
+		}
 
-	    Po_path.replace(index,size,"");
-	    old_name.replace(0,index+1,"");
-    }
+		Po_path.replace(index,size,"");
+		old_name.replace(0,index+1,"");
+	}
 
 	//newpath_parentname_myname
 	string Pn_path = newpath;
 	string new_name = newpath;
 	if(newpath == "/") {
-	    Pn_path = newpath;
-        new_name = newpath;
-        root_inumber = inode_base_idx;
-    }
-    else {
-       int size = Pn_path.size();
-       int index;
-       for (int i = size; i >= 0; i--){
-           if(Pn_path[i] == "/"){
-               index = i;
-               break;
-           }
-       }
+		Pn_path = newpath;
+		new_name = newpath;
+		root_inumber = inode_base_idx;
+	}
+	else {
+		int size = Pn_path.size();
+		int index;
+		for (int i = size; i >= 0; i--){
+			if(Pn_path[i] == "/"){
+				index = i;
+				break;
+			}
+		}
 
-	    Pn_path.replace(index,size,"");
-	    new_name.replace(0,index+1,"");
-    }
+		Pn_path.replace(index,size,"");
+		new_name.replace(0,index+1,"");
+	}
 
 	//name change
 	struct metadata p_meta;
-    struct dir_entry p_e;
-    int p_id = inode_finder(Pn_path);	
+	struct dir_entry p_e;
+	int p_id = inode_finder(Pn_path);	
 	int ind = indode_finder(oldpath);
 
 	pread(fd,&p_meta,sizeof(p_meta),p_id);
-    pread(fd,&p_e,sizeof(p_e),p_meta.data_ptr);
+	pread(fd,&p_e,sizeof(p_e),p_meta.data_ptr);
 
 	map<string,int>::iterator it;
-    for(it = map.begin(); it!=map.end(); it++){
-	    if(it == e.entry.find(my_name)){
-	        e.entry.erase(new_name);
-            e.entry.push_back(make_pair(new_name,ind));
+	for(it = map.begin(); it!=map.end(); it++){
+		if(it == e.entry.find(my_name)){
+			e.entry.erase(new_name);
+			e.entry.push_back(make_pair(new_name,ind));
 			break;
-        }
+		}
 	}
 	int nw1 = pwrite(fd, &p_e, sizeof(p_e), (off_t) p_meta.data_ptr);
 
@@ -128,13 +128,13 @@ int fs_rename (const char *oldpath, const char *newpath, unsigned int flags) {
 int fs_access (const char *path, int mask) {
 
 	struct metadata meta;
-    int ind = inode_finder(path);	
+	int ind = inode_finder(path);	
 
 	int nr1 = pread(fd, &meta, sizeof(meta), (off_t) ind);
 	if (mask == meta.mode){
-			return 0;
+		return 0;
 	}
-	
+
 	return -1;
 }
 
@@ -160,17 +160,17 @@ void *fs_init (struct fuse_conn_info *conn, struct fuse_config *cfg) {
 #ifdef MONITOR
 	monitor_init(&global_monitor);
 #endif
-        // file system stotrage space
-        if((fd = open("disk", O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1 ) {
-                printf("File open error.\n");
-                return NULL;
-        }
-	
+	// file system stotrage space
+	if((fd = open("disk", O_WRONLY | O_CREAT | O_TRUNC, 0644)) == -1 ) {
+		printf("File open error.\n");
+		return NULL;
+	}
+
 	//disk offset init
-        dioff.inode_bitmap_offset = 0;
-        dioff.data_bitmap_offset = 0;
-        dioff.inode_offset = 0;
-        dioff.data_block_offset = 0;
+	dioff.inode_bitmap_offset = 0;
+	dioff.data_bitmap_offset = 0;
+	dioff.inode_offset = 0;
+	dioff.data_block_offset = 0;
 
 
 	fs_mkdir("/", 0755);
