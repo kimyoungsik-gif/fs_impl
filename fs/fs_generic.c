ee00a51ee0992ed02775d658371346ca3533a62c
kimyoungsik-gif <addi97@naver.com>
2020-02-05 19:48:40 +0900

    gener

--- fs/fs_generic.c
+++ fs/fs_generic.c
@@ -16,7 +16,8 @@ struct monitor *global_monitor;
 
 int fs_getattr (const char *path, struct stat *stbuf, struct fuse_file_info *fi) {
 	struct metadata meta;
-	int ind = inode_finder(path);	
+	char* temp = (char*)path
+		int ind = inode_finder(path);	
 
 	int nr1 = pread(fd, &meta, sizeof(meta), (off_t) ind);
 	stbuf->st_mode = meta.mode;
@@ -31,13 +32,16 @@ int fs_getattr (const char *path, struct stat *stbuf, struct fuse_file_info *fi)
 
 int fs_utimens (const char *path, const struct timespec ts[2], struct fuse_file_info *fi) {
 
-	return 0;
+	char* temp = (char*)path
+		return 0;
 
 }
 
 int fs_chmod (const char *path, mode_t mode, struct fuse_file_info *fi) {
 	struct metadata meta;
-	int ind = inode_finder(path);	
+
+	char* temp = (char*)path
+		int ind = inode_finder(temp);	
 
 	int nr1 = pread(fd, &meta, sizeof(meta), (off_t) ind);
 	meta.mode = mode;
@@ -48,7 +52,8 @@ int fs_chmod (const char *path, mode_t mode, struct fuse_file_info *fi) {
 
 int fs_chown (const char *path, uid_t uid, gid_t gid, struct fuse_file_info *fi) {
 	struct metadata meta;
-	int ind = inode_finder(path);	
+	char* temp = (char*)path
+		int ind = inode_finder(temp);	
 	fi->fh = ind;
 
 	int nr1 = pread(fd, &meta, sizeof(meta), (off_t) ind);
@@ -60,11 +65,14 @@ int fs_chown (const char *path, uid_t uid, gid_t gid, struct fuse_file_info *fi)
 
 int fs_rename (const char *oldpath, const char *newpath, unsigned int flags) {
 	//oldpath_parentname_myname
-	string Po_path = oldpath;
-	string old_name = oldpath;
+	char* old = (char*)oldpath
+		char* new = (char*)newpath
+		string Po_path = old;
+	string old_name = old;
+
 	if(oldpath == "/") {
-		Po_path = oldpath;
-		old_name = oldpath;
+		Po_path = old;
+		old_name = old;
 		root_inumber = inode_base_idx;
 	}
 	else {
@@ -82,11 +90,11 @@ int fs_rename (const char *oldpath, const char *newpath, unsigned int flags) {
 	}
 
 	//newpath_parentname_myname
-	string Pn_path = newpath;
-	string new_name = newpath;
+	string Pn_path = new;
+	string new_name = new;
 	if(newpath == "/") {
-		Pn_path = newpath;
-		new_name = newpath;
+		Pn_path = new;
+		new_name = new;
 		root_inumber = inode_base_idx;
 	}
 	else {
@@ -105,31 +113,18 @@ int fs_rename (const char *oldpath, const char *newpath, unsigned int flags) {
 
 	//name change
 	struct metadata p_meta;
-<<<<<<< HEAD
 	struct dir_entry p_e;
 	int p_id = inode_finder(Pn_path);	
 	int ind = indode_finder(oldpath);
-=======
-    struct dir_entry p_e;
-    int p_id = inode_finder(Pn_path);	
-	int ind = inode_finder(oldpath);
->>>>>>> 8b3523f89cd0020e4d5bb1003cb7ba04a26d77cc
 
 	pread(fd,&p_meta,sizeof(p_meta),p_id);
 	pread(fd,&p_e,sizeof(p_e),p_meta.data_ptr);
 
 	map<string,int>::iterator it;
-<<<<<<< HEAD
-	for(it = map.begin(); it!=map.end(); it++){
-		if(it == e.entry.find(my_name)){
-			e.entry.erase(new_name);
-			e.entry.push_back(make_pair(new_name,ind));
-=======
-    for(it = p_e.begin(); it!=p_e.end(); it++){
-	    if(it == p_e.entry.find(old_name)){
-	        p_e.entry.erase(old_name);
-            p_e.entry.push_back(make_pair(new_name,ind));
->>>>>>> 8b3523f89cd0020e4d5bb1003cb7ba04a26d77cc
+	for(it = p_e.begin(); it!=p_e.end(); it++){
+		if(it == p_e.entry.find(old_name)){
+			p_e.entry.erase(old_name);
+			p_e.entry.push_back(make_pair(new_name,ind));
 			break;
 		}
 	}
@@ -141,7 +136,8 @@ int fs_rename (const char *oldpath, const char *newpath, unsigned int flags) {
 int fs_access (const char *path, int mask) {
 
 	struct metadata meta;
-	int ind = inode_finder(path);	
+	char* temp = (char*) path
+		int ind = inode_finder(temp);	
 
 	int nr1 = pread(fd, &meta, sizeof(meta), (off_t) ind);
 	if (mask == meta.mode){
